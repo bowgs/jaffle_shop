237329b456c27f766609ac4d98e18ca3e65b4d88
bowgs <joe.bowgett@gmail.com>
2024-12-31 14:25:43 +0000

    Added two new macros

---
+++ models/marts/core/int_orders__pivoted.sql
@@ -0,0 +1,32 @@
+{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
+WITH payments AS (
+    SELECT
+        *
+    FROM
+        {{ ref('stg_stripe__payments') }}
+),
+pivoted AS (
+    SELECT
+        order_id,
+        {%- for payment_method in payment_methods %}
+        SUM(
+            CASE
+                WHEN payment_method = '{{ payment_method }}' THEN amount
+                ELSE 0
+            END
+            ) AS {{ payment_method }}_amount
+        {%- if not loop.last -%},
+        {%- endif %}
+    {% endfor -%}
+    FROM
+        payments
+    WHERE
+        status = 'success'
+    GROUP BY
+        1
+)
+SELECT
+    *
+FROM
+    pivoted
